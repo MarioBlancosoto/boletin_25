6bcc4e6d738bf85f736cae0b05c7bfd0d642a98c
MarioBlancosoto <Mario@DESKTOP-NPV8TDI>
2017-04-21 11:28:36 +0200

    commit 1

---
+++ src/boletin_25/Boletin_25.java
@@ -0,0 +1,12 @@
+
+package boletin_25;
+
+
+public class Boletin_25 {
+
+    public static void main(String[] args) {
+       Ventana vent = new Ventana();
+       vent.setVisible(true);
+    }
+    
+}
